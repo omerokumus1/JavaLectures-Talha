f980edc722f93cbc1cd3785495fdcf9540218fac
omerokumus <omerokumus3@gmail.com>
2022-03-16 15:57:55 +0300

    binary search

--- src/Ch7_Arrays/Ch7_10_ArraySearch.java
+++ src/Ch7_Arrays/Ch7_10_ArraySearch.java
@@ -30,9 +30,81 @@ public class Ch7_10_ArraySearch {
         System.out.println("str contains f: " + charSearchInAString(str, 'f'));
         System.out.println("str contains k: " + charSearchInAString(str, 'k'));
         System.out.println("str contains j: " + charSearchInAString(str, 'j')); // case sentisive olduğu için false döner
+
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int e = 12;
+        System.out.println("nums contains " + e + ": " + binarySearch(nums, e));
+        e = 8;
+        System.out.println("nums contains " + e + ": " + binarySearch(nums, e));
+        e = 25;
+        System.out.println("nums contains " + e + ": " + binarySearch(nums, e));
+        e = -4;
+        System.out.println("nums contains " + e + ": " + binarySearch(nums, e));
+        e = 7;
+        System.out.println("nums contains " + e + ": " + binarySearch(nums, e));
+        e = 1;
+        System.out.println("nums contains " + e + ": " + binarySearch(nums, e));
+        e = 20;
+        System.out.println("nums contains " + e + ": " + binarySearch(nums, e));
+
+        e = 12;
+        System.out.println("nums contains " + e + ": " + binarySearch2(nums, e));
+        e = 8;
+        System.out.println("nums contains " + e + ": " + binarySearch2(nums, e));
+        e = 25;
+        System.out.println("nums contains " + e + ": " + binarySearch2(nums, e));
+        e = -4;
+        System.out.println("nums contains " + e + ": " + binarySearch2(nums, e));
+        e = 7;
+        System.out.println("nums contains " + e + ": " + binarySearch2(nums, e));
+        e = 1;
+        System.out.println("nums contains " + e + ": " + binarySearch2(nums, e));
+        e = 20;
+        System.out.println("nums contains " + e + ": " + binarySearch2(nums, e));
+
+
+
     }
 
-    private static boolean charSearchInAString(String str, char ch){
+    private static boolean binarySearch(int[] arr, int e) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+        while (true) {
+            mid = (start + end) / 2;
+            if (start + 1 == end){
+                return arr[start] == e || arr[end] == e;
+            }
+            if (arr[mid] == e) // eleman bulundu
+                return true;
+            else if (arr[mid] > e)
+                end = mid;
+            else
+                start = mid;
+        }
+
+    }
+    private static boolean binarySearch2(int[] arr, int e) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+        while (true) {
+            mid = (start + end) / 2;
+            if (start > end){
+                return false;
+            }
+            if (arr[mid] == e) // eleman bulundu
+                return true;
+            else if (arr[mid] > e)
+                end = mid-1;
+            else
+                start = mid+1;
+
+        }
+
+    }
+
+    private static boolean charSearchInAString(String str, char ch) {
         final int length = str.length();
         for (int i = 0; i < length; i++) {
             if (str.charAt(i) == ch)
@@ -42,7 +114,7 @@ public class Ch7_10_ArraySearch {
     }
 
 
-    private static boolean linearSearch(int[] arr, int e){
+    private static boolean linearSearch(int[] arr, int e) {
         for (int el :
                 arr) {
             if (el == e)
@@ -53,8 +125,7 @@ public class Ch7_10_ArraySearch {
     }
 
 
-
-    private static int[] createArray(){
+    private static int[] createArray() {
         int[] arr = new int[50];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i;
