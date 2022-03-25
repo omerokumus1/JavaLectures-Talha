b5d64c424c6799122df0df364943e41d6c12e6ee
omerokumus <omerokumus3@gmail.com>
2022-03-25 16:00:31 +0300

    insertion sort, new homeworks

--- src/Ch7_Arrays/Ch7_11_ArraySort.java
+++ src/Ch7_Arrays/Ch7_11_ArraySort.java
@@ -5,16 +5,52 @@ import java.util.Arrays;
 public class Ch7_11_ArraySort {
     public static void main(String[] args) {
         int[] nums = {2, 9, 5, 4, 8, 1, 6};
+        int[] nums2 = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 //        selectionSort(nums);
-        bubbleSort4(nums);
-//        System.out.println(Arrays.toString(nums));
+//        bubbleSort2(nums);
+        insertionSort2(nums);
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    private static void insertionSort2(int[] nums) {
+        int counter = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            slideElementLeft(nums, i);
+        }
+        System.out.println("Counter: " + counter);
+    }
+
+    private static void slideElementLeft(int[] nums, int i) {
+        for (int j = i; j >= 0 && nums[j + 1] < nums[j]; j--) {
+            swapConsecutiveElements(nums, j); // swap j and j+1
+            System.out.println(Arrays.toString(nums));
+//                counter++;
+        }
+    }
+
+    private static void insertionSort(int[] nums) {
+        int counter = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] < nums[i]) {
+                int j = i;
+                while (j >= 0 && nums[j + 1] < nums[j]) {
+                    swapConsecutiveElements(nums, j); // swap j and j+1
+                    System.out.println(Arrays.toString(nums));
+                    j--;
+                    counter++;
+                }
+            } else
+                counter++;
+        }
+        System.out.println("counter: " + counter);
     }
 
     private static void bubbleSort(int[] nums) {
         int counter = 0;
-        for (int i = 0; i < nums.length-1; i++) {
-            for (int j = 0; j < nums.length-1; j++) {
-                if (nums[j] > nums[j+1])
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[j] > nums[j + 1])
                     swapConsecutiveElements(nums, j);
                 counter++;
             }
@@ -25,9 +61,9 @@ public class Ch7_11_ArraySort {
 
     private static void bubbleSort2(int[] nums) {
         int counter = 0;
-        for (int i = 0; i < nums.length-1; i++) {
-            for (int j = 0; j < nums.length-1-i; j++) {
-                if (nums[j] > nums[j+1])
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1])
                     swapConsecutiveElements(nums, j);
                 counter++;
             }
@@ -35,12 +71,13 @@ public class Ch7_11_ArraySort {
         }
         System.out.println(counter);
     }
+
     private static void bubbleSort3(int[] nums) {
         int indexOfMin = findMinIndex(nums);
         int counter = 0;
         for (int i = 0; i < indexOfMin; i++) {
-            for (int j = 0; j < nums.length-1; j++) {
-                if (nums[j] > nums[j+1])
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[j] > nums[j + 1])
                     swapConsecutiveElements(nums, j);
                 counter++;
             }
@@ -48,13 +85,14 @@ public class Ch7_11_ArraySort {
         }
         System.out.println(counter);
     }
-   private static void bubbleSort4(int[] nums) {
+
+    private static void bubbleSort4(int[] nums) {
         int counter = 0;
         boolean isSorted;
-        for (int i = 0; i < nums.length-1; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             isSorted = true;
-            for (int j = 0; j < nums.length-1; j++) {
-                if (nums[j] > nums[j+1]) {
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
                     swapConsecutiveElements(nums, j);
                     isSorted = false;
                 }
@@ -79,11 +117,11 @@ public class Ch7_11_ArraySort {
 
     private static void swapConsecutiveElements(int[] nums, int i) {
         int temp = nums[i];
-        nums[i] = nums[i+1];
-        nums[i+1] = temp;
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
     }
 
-    private static void selectionSort(int[] arr){
+    private static void selectionSort(int[] arr) {
         int minIndex, temp;
         for (int i = 0; i < arr.length - 1; i++) {
             minIndex = i;
@@ -100,7 +138,7 @@ public class Ch7_11_ArraySort {
         }
     }
 
-    private static void selectionSort2(int[] arr){
+    private static void selectionSort2(int[] arr) {
         int minIndex;
         for (int startIndex = 0; startIndex < arr.length - 1; startIndex++) {
             // find min
@@ -111,13 +149,13 @@ public class Ch7_11_ArraySort {
         }
     }
 
-    private static void swapElements(int[] arr,  int startIndex, int minIndex) {
+    private static void swapElements(int[] arr, int startIndex, int minIndex) {
         int temp = arr[startIndex];
         arr[startIndex] = arr[minIndex];
         arr[minIndex] = temp;
     }
 
-    private static int findMinIndex(int[] arr, int startIndex){
+    private static int findMinIndex(int[] arr, int startIndex) {
         int minIndex = startIndex;
         for (int j = startIndex; j < arr.length; j++) { // sıralanmamış dizi içerisinde minimum elemanı bul
             if (arr[j] < arr[minIndex])
